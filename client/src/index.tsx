bd1c4ba61ba2f36b9ce20d8a769995104d0c491b
Daniel Digby <daniel.a.digby@gmail.com>
2021-09-04 17:55:49 +1000

    feat: configure docker-compose and build stages

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./tests/reportWebVitals";
-import "semantic-ui-css/semantic.min.css";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
             <App />
-        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
 );
