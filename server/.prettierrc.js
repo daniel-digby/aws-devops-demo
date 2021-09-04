1b35adc18823a5b40b9124cd3f864d2ccd9925ed
Daniel Digby <daniel.a.digby@gmail.com>
2021-09-04 14:57:14 +1000

    fix: simplify server demo by removing linting and formatting packages

--- server/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-// prettier.config.js or .prettierrc.js
-module.exports = {
-    endOfLine: "auto",
-    useTabs: false,
-    trailingComma: "es5",
-    tabWidth: 4,
-    semi: true,
-    singleQuote: false,
-};
--- server/README.md
+++
@@ -1,1 +0,0 @@
-# comp30022-it-project
