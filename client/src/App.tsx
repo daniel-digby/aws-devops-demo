25fb5c4dd1d4b1c2dd160ebea48762e140a8ccae
Daniel Digby <daniel.a.digby@gmail.com>
2021-09-06 15:46:54 +1000

    fix: adjust dockerfiles and add compose

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,7 @@ import React from "react";
 // import our views
 
 const App = (): JSX.Element => {
-    return (
-        <div>
-            hello world
-        </div>
-    );
+  return <div>hello world from client</div>;
 };
 
 export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080;
 require("dotenv").config();
 
 app.get("/", (_, res) => {
-    res.status(200).send("hello world");
+  res.status(200).send("hello world from server");
 });
 
 app.listen(port, () => console.log(`Running on http://localhost:${port}`));
